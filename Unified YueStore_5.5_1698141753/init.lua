cc7ff127b437530fba3386a07e785caa94d0d38e
XiaoBaiBZS <93860707+XiaoBaiBZS@users.noreply.github.com>
2023-10-24 21:14:39 +0800

    Add files via upload

---
+++ Unified YueStore_5.5_1698141753/init.lua
@@ -0,0 +1,15 @@
+appname="Unified YueStore"
+template="tool"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+appcode="20220504"
+appver="5.5"
+packagename="com.MyFusApp.UYS"
+
+--require "import" import"loadlayout" import"loadmenu" import"loadbitmap" 
